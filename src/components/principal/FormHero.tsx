6be0b821071741dd8994e4eecb9c12c23ab90a6b
alejooroncoy <adoa2705@gmail.com>
2024-11-14 22:53:59 -0500

    feat(feature/implementation-send-email): Update send email
    Update send email and api

---
+++ src/components/principal/FormHero.tsx
@@ -0,0 +1,62 @@
+import { useState, type FormEventHandler } from "react";
+
+const FormHero = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmitFormSendEmail: FormEventHandler<HTMLFormElement> = async (
+    event
+  ) => {
+    event.preventDefault();
+
+    setLoading(true);
+
+    const formData = new FormData(event.target as HTMLFormElement);
+
+    const email = formData.get("email");
+
+    await fetch("/api/send-email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+    });
+
+    setLoading(false);
+  };
+
+  return (
+    <form className="form form--hero" onSubmit={handleSubmitFormSendEmail}>
+      <input
+        name="email"
+        className="input input--hero"
+        type="email"
+        required
+        placeholder="Tu correo de empresa"
+      />
+      <button disabled={loading} className="button button--hero">
+        <span className="button__text">Solicitar acceso</span>
+        {loading ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            width="24"
+            height="24"
+            className="button__icon button__icon--loading"
+            strokeWidth="2"
+          >
+            <path d="M12 3a9 9 0 1 0 9 9"></path>
+          </svg>
+        ) : (
+          children
+        )}
+      </button>
+    </form>
+  );
+};
+
+export default FormHero;
---
+++ src/pages/api/send-email.ts
@@ -0,0 +1,17 @@
+export const prerender = false;
+
+import { COMPANY_NAME } from "@/constants";
+import { sendEmail } from "@/services/email";
+
+export const POST = async ({ request }) => {
+  const body = await request.json();
+
+  const { email } = body;
+
+  await sendEmail(email, `¡Hemos recibido tu solicitud para probar nuestra aplicación!`, `Agradecemos tu interés en ${COMPANY_NAME}. Actualmente, nos encontramos en una beta cerrada, te enviaremos un correo cuando estemos listos para que pruebes nuestra aplicación.
+    
+Saludos cordiales,
+El equipo de ${COMPANY_NAME}`);
+
+  return new Response("Email sent", { status: 200 });
+};
